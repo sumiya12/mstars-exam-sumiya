da1d8e1cb3e5c1eeb9eae68f53d20de44bc18311
sumiya <sumiya.udval@gmail.com>
2024-10-08 10:57:39 +0800

    change canvas

--- Back-end/routes/canvasRoute.js
+++ Back-end/routes/canvasRoute.js
@@ -1,4 +1,6 @@
-import { Router } from "express";
+import express from 'express';
+const { Router } = express;
+
 const router = Router();
 import { fetchBooksByCanvas, createNewCanvas } from "../controllers/controller.js";
 
